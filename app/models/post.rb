da67ff9036bb1db43b01ba6fb387f7129a6357b4
psarma89 <priyam.sarma@gmail.com>
2017-12-02 11:27:33 -0500

    adds solutiom

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments
+  # accepts_nested_attributes_for :comments, reject_if: :reject_comments
+  #
+  # def reject_comments(attributes)
+  #   attributes['content'].blank?
+  # end
+
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     # self.categories << category
+  #     self.post_categories.build(category: category)
+  #   end
+  # end
 
 end
